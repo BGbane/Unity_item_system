3a72faab588673ace22514d253dd922d804566a5
Bane Bane <bane.bravegiant@gmail.com>
2015-09-04 15:39:23 +0200

    Fader script added, item system started

---
+++ Proj/Assets/FaderEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaderEffect : MonoBehaviour {
+
+
+    public bool IsActive;
+    [Tooltip("Interval / Sekunds ")]
+    public float FaderInterval;
+    float nextActionTime = 0;
+    public float FadeSpeed;
+    [Range(0,1)]
+    public float Min;
+    [Range(0, 1)]
+    public float Max;
+
+
+	void Update () {
+
+        if(IsActive)
+            FaderAnimationPlay();
+	}
+
+
+    void FaderAnimationPlay()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += FaderInterval;
+            float randomAlpha = Random.Range(Min, Max);
+            float lerp = Mathf.PingPong(Time.time, FadeSpeed) / FadeSpeed;
+            this.gameObject.GetComponent<SpriteRenderer>().color = new Color(255f, 255f, 255f, Mathf.Lerp(Min, Max, lerp));
+        }
+    }
+
+      
+}
---
+++ Proj/Assets/ItemSystem/Item System/Scripts/Interfaces/IISObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IISObject {
+
+    string ISName { get; set; }
+    int ISValue { get; set; }
+    Sprite ISIcon { get; set; }
+    ISQuality ISQuality { get; set; }
+
+
+}
